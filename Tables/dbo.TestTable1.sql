48092ce866ec17d3ce8e4291f77bdd2ca6ddc865
unknown <jgsmith@JENNSQL.mountville.local>
2014-10-10 14:14:04 -0400

    Create a new test table

---
+++ Tables/dbo.TestTable1.sql
@@ -0,0 +1,6 @@
+CREATE TABLE [dbo].[TestTable1]
+(
+[TestColumn1] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[TestColumn2] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+) ON [PRIMARY]
+GO
