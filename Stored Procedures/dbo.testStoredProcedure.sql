964ce95e927aab296f4d0d977bb4bcdfa1cad60f
unknown <jgsmith@JENNSQL.mountville.local>
2014-10-10 14:56:53 -0400

    added new stored procedure

---
+++ Stored Procedures/dbo.testStoredProcedure.sql
@@ -0,0 +1,14 @@
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_NULLS ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [dbo].[testStoredProcedure]
+AS
+    -- Insert statements for procedure here
+	SELECT * from dbo.SALESLINE
+GO
