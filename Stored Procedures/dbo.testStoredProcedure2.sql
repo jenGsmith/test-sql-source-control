d4579199280cb7d565e1a73928888e7651037adb
unknown <jgsmith@JENNSQL.mountville.local>
2014-11-04 13:26:26 -0500

    added a new stored procedure to test with for user jengsmith

---
+++ Stored Procedures/dbo.testStoredProcedure2.sql
@@ -0,0 +1,15 @@
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_NULLS ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [dbo].[testStoredProcedure2]
+AS
+    -- Insert statements for procedure here
+	SELECT * from dbo.SALESLINE
+
+GO
