8c55fee0688846bd82f45162e33f0a46391b9051
Jennifer Smith <jgsmith@mountville.com>
2014-12-16 16:09:17 -0500

    test

--- Tables/dbo.TestTable1.sql
+++ Tables/dbo.TestTable1.sql
@@ -1,6 +1,7 @@
 CREATE TABLE [dbo].[TestTable1]
 (
 [TestColumn1] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-[TestColumn2] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+[TestColumn2] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[TestColumn3] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 ) ON [PRIMARY]
 GO
